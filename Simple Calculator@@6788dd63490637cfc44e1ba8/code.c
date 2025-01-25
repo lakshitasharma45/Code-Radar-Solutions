4b35580a112c83b9b2cf3d29cb756ca196383c9a
lakshitasharma45 <lakshitadhariwal@gmail.com>
2025-01-25 18:38:07 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,11 +4,16 @@ int main() {
     printf("");scanf("%d %d %c",&a,&b,&c);
     int sum=(a+b);int diff=(a-b);int product=(a*b);int div=(a/b);
     switch(c) {
-        case '+': printf("%d",sum);break;
-        case '-' :printf("%d",diff);break;
-        case '*' :printf("%d",product);break;
-        case '/' :printf("%d",div);break;
-        default :printf("error");break;
+        case '+':
+            printf("%d\n",sum);break;
+        case '-' :
+            printf("%d\n",diff);break;
+        case '*' :
+            printf("%d\n",product);break;
+        case '/' :
+            printf("%d\n",div);break;
+        default :
+            printf("error\n");break;
     }
     return 0;
 }    
